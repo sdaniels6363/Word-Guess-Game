e279ff0d3fe9e137c51a66c1ecbcf79be3a17529
Scott Daniels <sdaniels6363@gmail.com>
2019-07-29 16:13:03 -0400

    added the comprehensive list of players, using a combination of a dictionary and array, includes the images tied to each player.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,74 @@
-var playerNames = ["gerrard","alonso","firmino","salah","","","","","","","",""]
+// player array and picture information
+var players = [
+  { 
+    name: "alisson",
+    img: "assets/images/alisson.jpg"
+  },
+  { 
+    name: "firmino",
+    img: "assets/images/firmino.jpg"
+  },
+  { 
+    name: "mane",
+    img: "assets/images/mane.jpg"
+  },
+  { 
+    name: "rush",
+    img: "assets/images/rush.jpg"
+  },
+  { 
+    name: "alonso",
+    img: "assets/images/alonso.jpg"
+  },
+  { 
+    name: "gerrard",
+    img: "assets/images/gerrard.jpg"
+  },
+  { 
+    name: "milner",
+    img: "assets/images/milner.jpeg"
+  },
+  { 
+    name: "salah",
+    img: "assets/images/salah.jpg"
+  },
+  { 
+    name: "barnes",
+    img: "assets/images/barnes.jpg"
+  },
+  { 
+    name: "henderson",
+    img: "assets/images/hendo.jpg"
+  },
+  { 
+    name: "origi",
+    img: "assets/images/origi.jpg"
+  },
+  {
+    name: "alexander-arnold",
+    img: "assets/images/taa.jpg"
+  },
+  {
+    name: "carragher",
+    img: "assets/images/carragher.jpg"
+  },
+  {
+    name: "dalglish",
+    img: "assets/images/kenny.png"
+  },
+  {
+    name: "robertson",
+    img: "assets/images/robertson.jpg"
+  },
+  {
+    name: "van dijk",
+    img: "assets/images/vvd.png"
+  },
+]
+
+
+var guessesRemaining = 11 // allow 11 guesses per round.
+
+// pick a player at random, once selected, remove from pool so they're not picked again.
+
+
